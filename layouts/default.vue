9ec045d3cafe54f9c10eac1e08de3e9711334c6e
DevHumbleChris <dev.humblechris254@gmail.com>
2023-07-28 18:45:45 +0300

    added the tailwindcss and nuxt-icon

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  meta: [
+    { property: 'og:title', content: `Analyze-KE` },
+    { name: 'description', content: 'Analyze-KE is a Task Management Dashboard built with Nuxt 3 and TailwindCSS.' },
+    { property: 'og:description', content: 'Analyze-KE is a Task Management Dashboard built with Nuxt 3 and TailwindCSS.' },
+    { property: 'og:url', content: 'https://hounter-ke.vercel.app/' },
+    { name: 'twitter:title', content: 'Hounter - House Hunter' },
+    { name: 'twitter:description', content: 'Analyze-KE is a Task Management Dashboard built with Nuxt 3 and TailwindCSS.' },
+    { name: 'twitter:site', content: 'https://hounter-ke.vercel.app/' }
+  ],
+  title: `Hounter - House Hunter`,
+})
+</script>
+
+<template>
+  <main class="max-w-9xl font-fontFamily">
+    <slot/>
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-2xl">Analyze-KE is a Task Management Dashboard built with Nuxt 3 and TailwindCSS.</h1>
+</template>
