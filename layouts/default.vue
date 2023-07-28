81f4cc5b4b282bd6125f9413e06a78742d57a0fa
DevHumbleChris <dev.humblechris254@gmail.com>
2023-07-28 18:54:06 +0300

    fixed the page title

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,7 @@ useHead({
     { name: 'description', content: 'Analyze-KE is a Task Management Dashboard built with Nuxt 3 and TailwindCSS.' },
     { property: 'og:description', content: 'Analyze-KE is a Task Management Dashboard built with Nuxt 3 and TailwindCSS.' },
     { property: 'og:url', content: 'https://analyze-ke.vercel.app/' },
-    { name: 'twitter:title', content: 'Hounter - House Hunter' },
+    { name: 'twitter:title', content: 'Analyze-KE - Task Management Dashboard' },
     { name: 'twitter:description', content: 'Analyze-KE is a Task Management Dashboard built with Nuxt 3 and TailwindCSS.' },
     { name: 'twitter:site', content: 'https://analyze-ke.vercel.app/' }
   ],
